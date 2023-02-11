e426d7f0402cdf73ced17599824f9fc681c665bf
Batur Eren <baturerenbey@gmail.com>
2023-02-11 20:36:54 +0300

    Added background opacity

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const potatoContainerII = document.getElementById("potatoContainerII")
 const potatoContainerIII = document.getElementById("potatoContainerIII")
 const counterRight = document.getElementById("counter-right")
 const plus = document.getElementById("plus")
+const bodyimage = document.querySelector("body")
 
 window.onload = ()=>{
     counterRight.innerHTML = counter;
+    if (counter === 0){
+        bodyimage.style.setProperty('--bg-opacity', '0.00');
+    }
+    // kayıt edilmiş opacity'sini localden çeker
+    const savedBgOpacity = localStorage.getItem('bgOpacity');
+
+    // eğer değer boş değilse, css değerini ekler
+    if (savedBgOpacity !== null) {
+        bodyimage.style.setProperty('--bg-opacity', savedBgOpacity);
+    }
+
 }
 
 function potatoAnimation(){
@@ -35,12 +47,9 @@ function potatoAnimationIII(){
 }
 
 plus.addEventListener("click", () =>{
-    console.log(potatoAnimationRandomizer);
     counter++
     counterRight.innerHTML = counter
-    console.log(counter);
     localStorage.setItem("potatoCounter", counter);
-
     if (potatoAnimationRandomizer === 0){
         potatoAnimation()
     }else if (potatoAnimationRandomizer === 1){
@@ -51,5 +60,25 @@ plus.addEventListener("click", () =>{
         potatoAnimation()
         potatoAnimationRandomizer = 0
     }
-    
+
+    //background opacity'sini locale atar
+    updateBackgroundOpacity(counter);
+    const bgOpacity = getComputedStyle(bodyimage).getPropertyValue("--bg-opacity").trim();
+    localStorage.setItem("bgOpacity", bgOpacity);
 });
+
+function updateBackgroundOpacity(counter) {
+    const counterOpacityMap = {0: 0.00, 20: 0.01,40: 0.02, 60: 0.03, 80: 0.04, 100: 0.05, 120: 0.06, 140: 0.07, 160: 0.08, 180: 0.09, 200: 0.10
+      };
+      let opacity = 0.00;
+      for (let i in counterOpacityMap) {
+        if (counter > i) {
+          opacity = counterOpacityMap[i];
+        } else {
+          break;
+        }
+      }
+      
+      bodyimage.style.setProperty('--bg-opacity', opacity);
+    bodyimage.clientHeight;
+}
